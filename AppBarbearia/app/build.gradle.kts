266538fd4485f286d61284a2aed7b7ab64d41292
guilhermefsx <guylhermefsouza@gmail.com>
2023-11-01 23:55:42 -0300

    TelainicialcomLogo

--- AppBarbearia/app/build.gradle.kts
+++ AppBarbearia/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "devandroid.ferreira.appbarbearia"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "devandroid.ferreira.appbarbearia"
