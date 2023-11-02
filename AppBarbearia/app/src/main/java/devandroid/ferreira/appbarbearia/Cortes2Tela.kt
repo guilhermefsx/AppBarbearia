ff0b30b9f61494b14259b339c05557de6b211f8f
guilhermefsx <guylhermefsouza@gmail.com>
2023-11-02 02:03:50 -0300

    Tela2 voltando para a tela inicial

--- AppBarbearia/app/src/main/java/devandroid/ferreira/appbarbearia/Cortes2Tela.kt
+++ AppBarbearia/app/src/main/java/devandroid/ferreira/appbarbearia/Cortes2Tela.kt
@@ -1,11 +1,22 @@
 package devandroid.ferreira.appbarbearia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import devandroid.ferreira.appbarbearia.databinding.ActivityCortes2TelaBinding
+import devandroid.ferreira.appbarbearia.databinding.ActivityMainBinding
 
 class Cortes2Tela : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCortes2TelaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cortes2_tela)
+        binding =  ActivityCortes2TelaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Voltartela2.setOnClickListener {
+            val voltarTela2ParaTela1 = Intent(this,MainActivity:: class.java)
+            startActivity(voltarTela2ParaTela1)
+        }
     }
 }
--- AppBarbearia/app/src/main/java/devandroid/ferreira/appbarbearia/MainActivity.kt
+++ AppBarbearia/app/src/main/java/devandroid/ferreira/appbarbearia/MainActivity.kt
@@ -1,11 +1,20 @@
 package devandroid.ferreira.appbarbearia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import devandroid.ferreira.appbarbearia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Cortes.setOnClickListener {
+            val navegarCortes = Intent (this,Cortes2Tela::class.java)
+            startActivity(navegarCortes)
+        }
     }
 }
